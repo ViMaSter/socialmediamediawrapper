8b585b197547d830f3b1738b15c355bbc3727482
Vincent Mahnke <accounts@vimaster.de>
2015-06-10 21:13:30 +0200

    + Added correct video-type

--- inc/type/video.php
+++ inc/type/video.php
@@ -22,8 +22,24 @@
 	*/
 	a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
 	</style>
+	<style type="text/css">
+		body {
+			background-color: black;
+			width: 100vw;
+  			height: 100vh;
+  			display: flex;
+		}
+		video {
+			max-width: 100vw;
+  			max-height: 100vh;
+  			margin: auto;
+  			display: block;
+		}
+	</style>
 </head>
 <body>
-	<img src="<?php echo $directURL; ?>" alt="">
+	<video controls="" autoplay="" name="media">
+		<source src="<?php echo $directURL; ?>" type="video/mp4">
+	</video>
 </body>
 </html>
