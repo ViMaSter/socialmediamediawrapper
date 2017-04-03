366d0eea75032df631043769ffd4aa4920f88c8c
Vincent Mahnke <accounts@vimaster.de>
2017-04-03 20:57:03 +0200

    + Added special layout for text

---
+++ inc/type/text.php
@@ -0,0 +1,73 @@
+<?php
+	header('Content-Type: text/html; charset=utf-8');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+
+	<meta itemprop="image" content="<?php echo $directURL; ?>" />
+
+	<meta property="og:title" content="<?php echo $filename; ?>" />
+	<meta property="og:type" content="instapp:photo"<?php /* workaround for facebook */ ?> />
+
+	<meta property="og:url" content="<?php echo $canonialURL ?>" />
+	<meta property="og:image" content="<?php echo $directURL; ?>" />
+
+	<meta name="medium" content="image" />
+
+	<meta name="twitter:card" content="photo" />
+	<meta name="twitter:site" content="@ViMaSter" />
+	<meta name="twitter:creator" content="@ViMaSter" />
+	<meta name="twitter:image" content="<?php echo $directURL; ?>" />
+
+	<title><?php echo $filename; ?></title>
+	<style type="text/css">
+	/*	
+		http://meyerweb.com/eric/tools/css/reset/ 
+   		v2.0 | 20110126
+   		License: none (public domain)
+	*/
+	a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
+	</style>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<style type="text/css">
+		body {
+			background-color: #F7F7F7;
+		}
+
+		.wrapper {
+			font-family: 'Open Sans', sans-serif;
+			background-color: #E7E7E7;
+			color: #222;
+
+		   	font-size: 1.5vmax;
+		   	line-height: 1.7;
+    		padding: 3vmax 4vmax 5vmax;
+    		margin: 0 18vmax;
+
+			min-height: calc(100vh - 8vmax);
+		}
+
+		@media (min-width:320px)  { .wrapper { margin: 0 2vmax; } }
+		@media (min-width:480px)  { .wrapper { margin: 0 6vmax; } }
+		@media (min-width:600px)  { .wrapper { margin: 0 8vmax; } }
+		@media (min-width:801px)  { .wrapper { margin: 0 11vmax; } }
+		@media (min-width:1025px) { .wrapper { margin: 0 14vmax; } }
+		@media (min-width:1281px) { .wrapper { margin: 0 18vmax; } }
+
+		.wrapper br {
+			display: block;
+			margin: 0px 0px;
+		}
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+		<?php
+			echo nl2br(file_get_contents($filename));
+		?>
+	</div>
+</body>
+</html>
